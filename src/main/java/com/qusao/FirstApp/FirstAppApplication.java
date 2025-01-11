410fda9a3d07036b448a0277e7e73fc1827b3913
admin <admin@example.com>
2025-01-11 19:02:28 +0300

    INITIAL COMMIT

---
+++ src/main/java/com/qusao/FirstApp/FirstAppApplication.java
@@ -0,0 +1,25 @@
+package com.qusao.FirstApp;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@SpringBootApplication
+public class FirstAppApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstAppApplication.class, args);
+    }
+}
